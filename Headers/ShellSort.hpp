13240899bb51810805103ff37790aba36a47cc29
Lop6879 <dsshnyakin@yandex.ru>
2022-09-09 16:02:49 +0300

    Done BubbleSort, Created ShellSort

--- Headers/ShellSort.hpp
+++ Headers/ShellSort.hpp
@@ -11,7 +11,7 @@ class ShellSort : public ISort<T> {
             int n = seq->GetLength();
             for (gap = n/2 ; gap>0; gap/=2){
                 for (i = gap; i<n; i++){
-                    for (j = i - gap; j>=0 && seq->Get(j) > seq->Get(j+gap); j-=gap){
+                    for (j = i - gap; j>=0 && cmp(seq->Get(j), seq->Get(j+gap))>0; j-=gap){
                         tmp = seq->Get(j);
                         seq->Set(seq->Get(j), seq->Get(j + gap));
                         seq->Set(seq->Get(j+gap), tmp);
