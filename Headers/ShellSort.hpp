51453e7c389683c7903daa45b127da78aced275b
Lop6879 <dsshnyakin@yandex.ru>
2022-09-20 14:54:03 +0300

    Done with ShellSort

--- Headers/ShellSort.hpp
+++ Headers/ShellSort.hpp
@@ -9,16 +9,30 @@ class ShellSort : public ISort<T> {
         Sequence<T>* Sort(Sequence<T>* seq, int (*cmp)(T,T)){
             int gap, i, j, tmp;
             int n = seq->GetLength();
-            for (gap = n/2 ; gap>0; gap/=2){
-                for (i = gap; i<n; i++){
+            for (gap = n/2 ; gap > 0; gap/=2){
+                for (i = gap; i < n; i++){
                     for (j = i - gap; j>=0 && cmp(seq->Get(j), seq->Get(j+gap))>0; j-=gap){
                         tmp = seq->Get(j);
-                        seq->Set(seq->Get(j), seq->Get(j + gap));
-                        seq->Set(seq->Get(j+gap), tmp);
+                        seq->Set(j, seq->Get(j + gap));
+                        seq->Set(j+gap, tmp);
                     }
                 }
             }
             return seq;
         }
+
+/*
+void shellsort (int v[], int n)
+{
+int gap, i, j, temp;
+for (gap = n/2; gap > 0; gap /= 2)
+for (i = gap; i < n; i++)
+for (j = i - gap; j >= 0 && v[j] > v[j + gap]; j -= gap) {
+temp = v[j];
+v[j] = v[j + gap];
+v[j + gap] = temp;
+}
+}
+*/
 
 };
