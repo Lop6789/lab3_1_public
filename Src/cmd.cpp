edd3bf871d969f23fd9519fa08ca430965fa3abd
Lop6879 <dsshnyakin@yandex.ru>
2022-10-03 10:18:59 +0300

    W

--- Src/cmd.cpp
+++ Src/cmd.cpp
@@ -16,6 +16,7 @@ void CheckSrc(int& argc, char* argv[]){
     ISort<int>* sort1 = nullptr;
     ISort<int>* sort2 = nullptr;
     ISort<int>* sort3 = nullptr;
+    // ISort<int>** isorts = new ISort<int>()[argc];
     if(fin.is_open()) cout << "Success!" << endl;
     while (getline(fin, line)){
         sorts->Append(line);
@@ -28,7 +29,16 @@ void CheckSrc(int& argc, char* argv[]){
             // cout << argv[i] << endl;
             // cout << "YES" << endl;
             //Create()
-            // if (argv[i] == "-bs") sort1 = newBubbleSort<int>*
+            // if (argv[i] == "-bs") sort1 = new BubbleSort<int>*
+            // if (argv[i] == "-qs") sort1 = new QuickSort<int>*
+            // if (argv[i] == "-shs") sort1 = new SHellSort<int>*
+            // switch (argv[i]){
+            //     case "-bs":
+            //         {   
+            //             sort1 = new BubbleSort<int>();
+            //             break;
+            //         }
+            // }
         }
     }
 
